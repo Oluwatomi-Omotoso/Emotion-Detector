d8bc4ce7d569d8cc12b4139294ad091dd780a20d
Oluwatomi-Omotoso <157227455+Oluwatomi-Omotoso@users.noreply.github.com>
2025-11-02 18:03:32 +0100

    Update app.py

--- OMOTOSO_23CG034133_Emotion_detector_web_app/app.py
+++ OMOTOSO_23CG034133_Emotion_detector_web_app/app.py
@@ -46,8 +46,7 @@ if os.path.exists(MODEL_PATH):
         model = None
 else:
     st.info(
-        "Oops Tomi's model might not be available at the moment.
-        Not to worry, he's set up DataFace as a substitute (almost as if he knew this could happen)."
+        "No local model found. Seems Tomi's model's doww. Not to worry he's set up Dataface as a substitute."
     )
 
 
@@ -194,6 +193,7 @@ if st.button("Show recent usage (last 10)"):
             st.write(f"{r[0]} | {r[1]} | {r[2]} | {r[4]}")
     else:
         st.write("No usage logged yet.")
+
 
 
 
